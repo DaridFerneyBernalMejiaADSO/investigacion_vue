7cca1af770de348b92e6f5e7702a75b64d9ed3a5
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2023-03-15 06:00:58 -0500

    v-model

--- compoonent/my-component.js
+++ compoonent/my-component.js
@@ -1,9 +1,7 @@
 export default{
     data(){
         return{
-            basico:`<strong style="color:greem;">apolo</strong>`,
-            intemedio:`<strong style="color:orange;">artemis</strong>`,
-            avanzado: `<strong style="color:blue;">sputnik</strong>`
+            mensaje:"lorem"
         }
     }
 }
