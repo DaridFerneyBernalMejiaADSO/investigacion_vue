46849c68c5bdb71687e8a34d687807891dd2d1f0
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2023-03-20 19:17:14 -0500

    v-bind

--- compoonent/my-component.js
+++ compoonent/my-component.js
@@ -1,11 +1,13 @@
 export default{
     data(){
         return{
-            lista:[
-                "apolo",
-                "alrtemik",
-                "sputnik"
-            ]
+            lista:{
+                Imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
+                texto: "Diferencias entre vue2 y vue3"
+
+            }
+                
+            
         }
     }
 }
