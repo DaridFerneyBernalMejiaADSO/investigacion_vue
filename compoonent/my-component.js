2c94e8354b09825824546edb0300767758f15b24
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2023-03-20 19:30:27 -0500

    v-on

--- compoonent/my-component.js
+++ compoonent/my-component.js
@@ -2,9 +2,11 @@ export default{
     data(){
         return{
             lista:{
+                atributo:"src",
                 Imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
+                atributo2: "alt",
                 texto: "Diferencias entre vue2 y vue3"
-//das
+
             }
                 
             
