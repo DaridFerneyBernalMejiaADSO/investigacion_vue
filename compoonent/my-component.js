d16233b40fb94052dc8af42d53814b362b14f34a
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2023-03-20 19:23:21 -0500

    v-bind

--- compoonent/my-component.js
+++ compoonent/my-component.js
@@ -4,7 +4,7 @@ export default{
             lista:{
                 Imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
                 texto: "Diferencias entre vue2 y vue3"
-
+//das
             }
                 
             
